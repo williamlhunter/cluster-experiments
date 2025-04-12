a66f7deb1b4fea30917bfd70b338451f373e0645
Bill Hunter <bill@williamlhunter.com>
2025-04-12 13:09:36 -0600

    initial commit

---
+++ cluster.md
@@ -0,0 +1,13 @@
+#cluster
+##load balancing
+ - Learn to make docker containers
+   * hello-go
+ - write simple webservice
+   * takes in number
+   * does something computationally expensive (bcrypt?)
+   * returns result
+ - containerize webservice
+ - run on cluster
+ - load test
+   * scalability
+   * high availability
---
+++ hello-go/cmd/hello/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you requested: %s", r.URL.Path)
+	log.Printf("Received request for path: %s", r.URL.Path)
+}
+
+func main() {
+	var addr string = ":8180"
+	handler := http.HandlerFunc(HelloServer)
+	if err := http.ListenAndServe(addr, handler); err != nil {
+		log.Fatalf("Could not listen on port %s %v", addr, err)
+	}
+}
